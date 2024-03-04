1d68be2c4844f1bf0635e86679c183994bad0698
G-Prasath <guruprasathmsc@gmail.com>
2024-03-04 15:53:58 +0530

    Email send Page Done

--- app/email/send/page.tsx
+++ app/email/send/page.tsx
@@ -35,6 +35,8 @@ const page = () => {
     const [open, setOpen] = useState(false);
     const [toggle, setToogle] = useState(false)
     const [toggleHover, setToggleHover] = useState(false)
+    const [check, setCheck] = useState(false)
+
 
 
     const handletabClick = (arg: any) => {
@@ -43,6 +45,9 @@ const page = () => {
     const chooseOprions = (e: any) => {
         setDropdownOptions(e.target.textContent)
         setDropdown(false)
+    }
+    const handleCheck = (e: any) => {
+        setCheck(e.target.checked)
     }
 
 
@@ -186,13 +191,47 @@ const page = () => {
                         <div>
                             <textarea cols={30} rows={10} className='w-full border-l-[1px] border-r-[1px] border-b-[1px] border-l-hash_clr border-r-hash_clr border-b-hash_clr outline-none p-[10px] rounded-b-md' placeholder='Compose Your Mail'></textarea>
                         </div>
+
+
+                        {/* --------------------- File PUSH ---------------  */}
+                        <div className={`relative flex justify-between items-center max-md:flex-col ${tabActive !== 'T2' ? 'hidden' : ''}`}>
+                            <div className='flex items-center gap-x-2 max-w-max'>
+                                <input type="checkbox" onChange={(e) => handleCheck(e)} />
+                                <p>Dynamic Messages</p>
+                            </div>
+
+                            <div className={`min-w-[300px] max-md:w-full ${check ? 'fade_top' : 'hidden'}`}>
+                                <button className="inputbox_style flex items-center justify-between overflow-hidden w-full" type="button" onClick={() => setDropdown(!dropdown)}>{dropdownOptions} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg>
+                                </button>
+
+
+                                <div className={`z-10 bg-white divide-y rounded-lg shadow w-full text-secondary ${dropdown ? 'fade_left' : 'absolute h-[0px] overflow-hidden duration-500'}`}>
+                                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                                        <li>
+                                            <a href="javascript:;" className="block px-4 py-2 hover:bg-hole_body text-secondary" onClick={(e) => chooseOprions(e)}>Dashboard</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;" className="block px-4 py-2 hover:bg-hole_body text-secondary" onClick={(e) => chooseOprions(e)}>Settings</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;" className="block px-4 py-2 hover:bg-hole_body text-secondary" onClick={(e) => chooseOprions(e)}>Earnings</a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        {/* --------------------- File PUSH ---------------  */}
+
                     </div>
                     {/* -------- Email Composer End -------------  */}
 
                 </div>
 
-                {/* ------------------ Box 3 -------------- */}
-                <div className='hole_box shadow-boxShadow-profileShadow hover:shadow-textboxShadow duration-300'>
+                {/* ------------------ Normal Push Box 3 -------------- */}
+                <div className={`hole_box shadow-boxShadow-profileShadow hover:shadow-textboxShadow duration-300 ${tabActive !== 'T1' ? 'hidden' : ''}`}>
                     <div className='text-[16px] jost font-[500] text-hash_clr px-[20px] py-[15px] border-b-[1px] border-b-hash_clr'>
                         <p>Emails</p>
                     </div>
@@ -203,6 +242,21 @@ const page = () => {
                         </div>
                         <div className='w-1/2 max-md:w-full max-md:px-[20px] flex justify-center items-center text-secondary'>
                             <p>Note : You can paste maximum 10,000 emails in a single queue. It should be one by one.</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* ----------------------- File Push Box 3 -----------------  */}
+                <div className={`hole_box shadow-boxShadow-profileShadow hover:shadow-textboxShadow duration-300 ${tabActive !== 'T2' ? 'hidden' : ''}`}>
+                    <div className='text-[16px] jost font-[500] text-hash_clr px-[20px] py-[15px] border-b-[1px] border-b-hash_clr'>
+                        <p>File Upload</p>
+                    </div>
+                    <div className='flex h-auto max-md:flex-col w-full'>
+                        <div className='w-full max-md:w-full p-[20px]'>
+                            <p className='text-[14px] font-semibold'>Choose File</p>
+                            <div className='w-full overflow-hidden relative'>
+                                <input type="file" className='border-[1px] border-hash_clr p-[5px] rounded-md  w-full' />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -236,7 +290,7 @@ const page = () => {
                         <div className='flex items-center'>
 
                             <div className={`absolute right-[-80px] max-md:right-0 -top-6 bg-card_hover_bg text-white py-[5px] rounded-md px-[10px] flex justify-center ${toggleHover ? 'scale-1 duration-500' : 'scale-0'}`}>
-                                 <div className='w-[10px] h-[10px] bg-card_hover_bg absolute -rotate-45 -bottom-[5px] max-md: right-[40px]'></div>   
+                                <div className='w-[10px] h-[10px] bg-card_hover_bg absolute -rotate-45 -bottom-[5px] max-md: right-[40px] '></div>
                                 <p className='text-[14px]'>Enable if you want schedule the message</p>
                             </div>
 
